5c60bd85c3a68bb8d0512e301feb68848fd09aa6
MoreBlood <new.shtirlic@mail.ru>
2016-06-23 23:44:20 +0300

    portfolio added
    raw

---
+++ dw/js/main.js
@@ -0,0 +1,51 @@
+
+		/*$(document).ready(function(){
+			var touch = $('.hidebtn');
+		    var menu = $('#nav');
+			var btn = $('#search_slide');
+		 
+		    $(touch).on('click', function(e) {
+		        e.preventDefault();
+		        menu.slideToggle();
+		    });*/
+			
+		    /*$(window).resize(function(){
+		        var wid = $(window).width();
+		        if(wid > 767 && menu.is(':hidden')) {
+		            menu.removeAttr('style');
+		        }
+		    });
+		});*/
+
+	$(document).ready(function(){
+
+		var target = $('.topNav').position();
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > target.top){
+            $('.topNav').css({position: 'fixed', marginTop: '-145px'});
+            //$('.topNav').addClass( "nav-deselected");
+            $('.top-btn').css({opacity: 1});
+        }
+		else if ($(window).scrollTop() < target.top){
+            $('.topNav').css({position: '', marginTop: '0px'});
+            //$('.topNav').removeClass( "nav-deselected");
+            $('.top-btn').css({opacity: 0});};
+    });
+});
+
+
+$(document).ready(function() {
+	
+
+	$("#form").bind('submit', function() {
+		//устанавливаем событие отправки для формы с id=form
+		var form_data = $(this).serialize(); //собераем все данные из формы
+		$.ajax({
+			type: "POST", //Метод отправки
+			url: "mail.php", //путь до php фаила отправителя
+			data: form_data
+			});
+	});
+	
+});
---
+++ dw/js/navigation.js
@@ -0,0 +1,81 @@
+		//Get Sections top position
+		function getTargetTop(elem){
+			
+			//gets the id of the section header
+			//from the navigation's href e.g. ("#html")
+			var id = elem.attr("href");
+
+			//Height of the navigation
+			var offset = 0;
+
+			//Gets the distance from the top and 
+			//subtracts the height of the nav.
+			return $(id).offset().top - offset;
+		}
+
+		//Smooth scroll when user click link that starts with #
+		$('#nav a[href^="#"]' ).click(function(event) {
+			
+			//gets the distance from the top of the 
+			//section refenced in the href.
+			var target = getTargetTop($(this));
+			var mobile_scroll_fix = 0;
+			if ($( document ).width() < 768) mobile_scroll_fix = 90;
+			if ($( document ).width() < 1240 && $( document ).width() > 768) mobile_scroll_fix = 210;
+
+			//scrolls to that section.
+			$('html, body').animate({scrollTop:target - mobile_scroll_fix}, 500);
+
+			//prevent the browser from jumping down to section.
+			event.preventDefault();
+
+		});
+
+		//Pulling sections from main nav.
+		var sections = $('.leftside-blocks a[href^="#"]');
+
+		// Go through each section to see if it's at the top.
+		// if it is add an active class
+		function checkSectionSelected(scrolledTo){
+			
+			//How close the top has to be to the section.
+			var threshold = 100;
+
+			var i;
+
+			for (i = 0; i < sections.length; i++) {
+				
+				//get next nav item
+				var section = $(sections[i]);
+
+				//get the distance from top
+				var target = getTargetTop(section);
+				
+				//Check if section is at the top of the page.
+				if (scrolledTo > target - threshold && scrolledTo < target + threshold) {
+					//remove all selected elements
+					sections.removeClass("active");
+
+					//add current selected element.
+					section.addClass("active");
+
+				}
+				if($(window).scrollTop() + $(window).height() == $(document).height()) { //fix for small last div
+					//remove all selected elements
+					sections.removeClass("active");
+
+					//add current selected element.
+					section.addClass("active");
+				}
+
+			};
+		}
+
+
+		//Check if page is already scrolled to a section.
+		checkSectionSelected($(window).scrollTop());
+
+		$(window).scroll(function(e){
+			checkSectionSelected($(window).scrollTop());
+
+		});
