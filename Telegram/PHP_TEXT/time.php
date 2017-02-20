301a11dc803e2c484d00324e635af55ab73fa216
MoreBlood <new.shtirlic@mail.ru>
2017-02-20 16:56:35 +0300

    Big update and son fixes

--- Telegram/PHP_TEXT/time.php
+++ Telegram/PHP_TEXT/time.php
@@ -1,34 +1,23 @@
-<?php
+﻿<?php
+include 'index.php';
 date_default_timezone_set("Europe/Minsk");
 $t=time();
 
-function type_of_day_rus($shift)
-{
-    global $t;
-    echo date("G:i", $t);
-    if ((date('N', $t - strtotime('+' .$shift . 'day', strtotime($t))) >= 6)) return "Выходной";
+function ConvertToASCII ($str){
+    $str_ascii = "";
+    for ($i = 0; $i < mb_strlen($str); $i++){
+        $str_ascii .= ord($str[$i]) . " ";
 
-    else return "Рабочий";
+    }
+    return $str_ascii;
 }
 
 
-function convert_time ($time){
-
-     return strtotime("1970-01-01 $time UTC");
 
 
-}
-$bus_times = array("6:30", "7:30", "21:22");
 
-function type_of_message($mesage){
-    $words = explode($mesage);
-    if (count($words) == 1) return "one";// остановка (поиск), номер остановки, автобус, да
-    if (count($words) == 2) return "two";// пары остановка и номер автобуса, автобус остановка,
-    if (count($words) == 3) return "three";// точное задание параметров о:а:в и вариации
 
-}
-
-echo convert_time("00:00");
+echo answer_for_bus_stop(try_to_find_from_all(RemoveKeyWord("транспорт", "цум транспорт")),"Туда", type_of_day_rus_now());
 
 
 ?>
